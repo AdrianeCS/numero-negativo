7c00292fedb33849ce1f98511bf135960a742c6b
adriane <adrianedacosta21@gmail.com>
2022-04-19 14:56:53 -0300

    finalizar

---
+++ Program.cs
@@ -0,0 +1,8 @@
+﻿int numero;
+Console.Write("digite um numero>:...");
+numero = Convert.ToInt32(Console.ReadLine());
+
+if (numero <0)
+{
+  Console.WriteLine("voce digitou um numero negativo");
+ }
